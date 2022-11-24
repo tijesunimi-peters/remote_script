6d1b429094538ae4de47b0f3a60cce421cbec456
Tijesunimi Peters <tijesunimipeters+github@gmail.com>
2022-11-24 14:25:05 -0600

    Update startup script

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
 
-docker run --privileged --name=main -p 2200:22 -d -v /var/run/docker.sock:/var/run/docker.sock remote
+docker run --privileged --name=main -p 2200:22 -p 3000:3000 -p 3050:3050 -p 5000:5000 -p 5001:5001 -p 5555:5555 -p 6379:6379 -p 8081:8081 -p 27017:27017 -d remote
